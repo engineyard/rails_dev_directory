8c73e1dad77a468ccca62192699fb42e5108a96d
Paul Campbell <paul@unknown-00-26-bb-13-44-d3.config>
2009-10-14 18:06:41 +0100

    use factory_girl from github

--- config/environments/cucumber.rb
+++ config/environments/cucumber.rb
@@ -19,4 +19,4 @@ config.gem 'cucumber',    :lib => false,        :version => '>=0.3.103' unless F
 config.gem 'webrat',      :lib => false,        :version => '>=0.5.0' unless File.directory?(File.join(Rails.root, 'vendor/plugins/webrat'))
 config.gem 'rspec',       :lib => false,        :version => '>=1.2.8' unless File.directory?(File.join(Rails.root, 'vendor/plugins/rspec'))
 config.gem 'rspec-rails', :lib => false,        :version => '>=1.2.7.1' unless File.directory?(File.join(Rails.root, 'vendor/plugins/rspec-rails'))
-config.gem 'factory_girl', :lib => 'factory_girl', :version => '1.2.2'
+config.gem 'thoughtbot-factory_girl', :lib => 'factory_girl', :version => '1.2.2', :source => 'http://gems.github.com'
