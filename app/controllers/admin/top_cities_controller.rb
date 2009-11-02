2ad0e629b52360debd121e2567caa22188f38a28
Paul Campbell <paul@rslw.com>
2009-11-02 18:12:06 +0000

    managing top cities

--- app/controllers/admin/top_cities_controller.rb
+++ app/controllers/admin/top_cities_controller.rb
@@ -1,3 +1,11 @@
 class Admin::TopCitiesController < ApplicationController
   inherit_resources
+  
+  def create
+    create! { admin_top_cities_path }
+  end
+                               
+  def update                   
+    update! { admin_top_cities_path }
+  end
 end
