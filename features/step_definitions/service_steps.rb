21e1f14c0350409474ccf695e79fdad5c577fe71
Rob Holland <rob@the-it-refinery.co.uk>
2009-11-05 10:12:47 +0000

    Fix bug which produced services with the wrong names from feature.

--- features/step_definitions/service_steps.rb
+++ features/step_definitions/service_steps.rb
@@ -13,21 +13,21 @@ Given /^the following services:$/ do |services|
 end
 
 Given /^primary services "([^\"]*)"$/ do |types|
-  types = types.split(',')
+  types = types.split(/, ?/)
   types.each do |type|
     Service.make(:name => type)
   end
 end
 
 Given /^secondary services "([^\"]*)"$/ do |types|
-  types = types.split(',')
+  types = types.split(/, ?/)
   types.each do |type|
     Service.make(:name => type)
   end
 end
 
 Given /^pre checked services "([^\"]*)"$/ do |types|
-  types = types.split(',')
+  types = types.split(/, ?/)
   types.each do |type|
     Service.make(:name => type, :checked => true)
   end
