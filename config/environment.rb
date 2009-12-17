0ccd38f7ac1129c15e745c393ba44e1d4050fa63
Paul Campbell <paul@Paul-Campbells-MacBook-Pro.local>
2009-12-17 23:35:11 +0000

    users form should just post to users path

--- config/environment.rb
+++ config/environment.rb
@@ -45,7 +45,7 @@ Rails::Initializer.run do |config|
   # Run "rake -D time" for a list of tasks for finding time zone names.
   config.time_zone = 'UTC'
   
-  config.action_mailer.default_url_options = { :host => "www.railsdevelopment.com" }
+  config.action_mailer.default_url_options = { :host => "freelance.railsdevelopment.com" }
 
   # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
   config.i18n.load_path += Dir[File.join(RAILS_ROOT, 'config', 'locales', '**', '*.{rb,yml}')]
