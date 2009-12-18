47ceed6b7bb64148652309ec81ef1ef57e26eadc
Paul Campbell <paul@Paul-Campbells-MacBook-Pro.local>
2009-12-18 01:05:02 +0000

    sort categories on provider show

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def show
     @homepage = Homepage.first
     @page = Page.find_by_url('home')
-    @general_category = ServiceCategory.find_by_name('General')
+    @general_category = ServiceCategory.find_by_name(config[:general_service])
     @general_services = @general_category ? @general_category.services : []
     @top_services = Service.priority(1).reject_category(@general_category)
     @mid_services = Service.priority(2).reject_category(@general_category)
--- app/controllers/my/dashboard_controller.rb
+++ app/controllers/my/dashboard_controller.rb
@@ -5,7 +5,7 @@ class My::DashboardController < ApplicationController
       @provider = current_user.provider if logged_in?
     else
       @page = Page.find_by_url('home')
-      @general_category = ServiceCategory.find_by_name('General')
+      @general_category = ServiceCategory.find_by_name(config[:general_service])
       @general_services = @general_category ? @general_category.services : []
       @top_services = Service.priority(1).reject_category(@general_category)
       @mid_services = Service.priority(2).reject_category(@general_category)
