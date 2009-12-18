c7fb4c2855f1cc5addb54046d3eb79aa117cd22c
Paul Campbell <paul@Paul-Campbells-MacBook-Pro.local>
2009-12-18 01:46:38 +0000

    add whitelist

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -15,6 +15,10 @@ class AvatarUploader < CarrierWave::Uploader::Base
   def store_dir
     "system/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
+  
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
 
   # Provide a default URL as a default if there hasn't been a file uploaded
   #     def default_url
