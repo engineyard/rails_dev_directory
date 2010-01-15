c31c58ea349aa9c7b7850fed94804aba34bb3b48
Paul Campbell <paul@Paul-Campbells-MacBook-Pro.local>
2010-01-15 20:28:31 +0000

    should probably order them randomly

--- app/helpers/my/sittings_helper.rb
+++ app/helpers/my/sittings_helper.rb
@@ -2,7 +2,7 @@ module My::SittingsHelper
   
   def setup_sitting(sitting)
     if sitting.responses.size < sitting.quiz.questions_per_quiz
-      sitting.quiz.questions.all(:limit => sitting.quiz.questions_per_quiz).each do |question|
+      sitting.quiz.questions.all(:limit => sitting.quiz.questions_per_quiz, :order => "RAND()").each do |question|
         sitting.responses.build(:question => question) if sitting.responses.collect(&:question).not.include?(question)
       end
     end
