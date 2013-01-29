e9db062987c67e5cacd6d0a82ef466cb91f99488
GerlandoP <gerlando@gmail.com>
2013-01-29 11:50:55 -0800

    updated rails version

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.15' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.16' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -40,7 +40,7 @@ Rails::Initializer.run do |config|
   # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
   # Run "rake -D time" for a list of tasks for finding time zone names.
   config.time_zone = 'UTC'
-  
+
   config.action_mailer.default_url_options = { :host => "www.railsdevelopment.com" }
 
   # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
